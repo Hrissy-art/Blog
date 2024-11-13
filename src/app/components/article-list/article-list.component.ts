9cab9f122cb6f62238652d00fce1c9afeefdaa14
Hristina <hristinastankova89@gmail.com>
2024-11-13 17:04:21 +0100

    changes with async pipes

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -15,12 +15,12 @@ import { Observable } from 'rxjs';
   styleUrl: './article-list.component.scss',
 })
 export class ArticleListComponent implements OnInit {
-  articles$!: Observable<Article[]>;
+  articles!: Observable<Article[]>;
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-    this.articles$ = this.apiService.getArticles();
+    this.articles = this.apiService.getArticles();
   }
 
   handleLike(article: Article) {
